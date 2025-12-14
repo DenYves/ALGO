09c644b824d8cde5a15d1c8c86e4851613109045
MathGod20 <leruursu@gmail.com>
2025-12-14 09:52:19 +0100

    test

---
+++ main.py
@@ -0,0 +1,157 @@
+import sys
+import time
+import os
+from collections import deque
+from openpyxl import Workbook
+from solver_methods import bfs_on_new_graph
+
+def read_instance(filename):
+
+    try:
+        with open(filename, "r") as f:
+            tokens = f.read().split()
+    except FileNotFoundError:
+        print(f"Error: file '{filename}' not found.", file=sys.stderr)
+        return None
+
+    # we use a iterator
+    it = iter(tokens)
+    try:
+        n = int(next(it))
+        m = int(next(it))
+        T = int(next(it))
+        D = int(next(it))
+
+        # we index from 0
+        s_a = int(next(it)) - 1
+        t_a = int(next(it)) - 1
+        s_b = int(next(it)) - 1
+        t_b = int(next(it)) - 1
+    except StopIteration:
+        print("Error: input ended unexpectedly in the header.", file=sys.stderr)
+        return None
+
+    # We create the adjacency list
+    adj = [[] for _ in range(n)]
+    for _ in range(m):
+        try:
+            u = int(next(it)) - 1
+            v = int(next(it)) - 1
+        except StopIteration:
+            print("Error: input ended unexpectedly in the edge list.", file=sys.stderr)
+            return None
+
+        adj[u].append(v)
+        adj[v].append(u)
+
+    return n, m, T, D, s_a, t_a, s_b, t_b, adj
+
+
+# converts the adjacency matrix into a distance matrix
+def compute_distance_matrix(n, adj):
+
+    dist = [[-1] * n for _ in range(n)]
+
+    for s in range(n):
+        q = deque()
+        q.append(s)
+        dist[s][s] = 0
+
+        while q:
+            u = q.popleft()
+            d_u = dist[s][u]
+            for v in adj[u]:
+                if dist[s][v] == -1:
+                    dist[s][v] = d_u + 1
+                    q.append(v)
+
+    return dist
+
+
+if __name__ == "__main__":
+    # the files that we want to run
+    folder = "testcases"
+    files = sorted(f for f in os.listdir(folder) if f.endswith("honey500.in"))
+
+    # creating the workbook
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Results"
+
+    # the header row
+    ws.append([
+        "File", "n", "m", "T", "D", "k - algorithm", "k - solution",
+        "Expected Time (s)", "Actual Time (s)", "Difference (Actual - Expected)"
+    ])
+
+    for filename in files:
+        full_path = os.path.join(folder, filename)
+
+        print(f"\n===== FILE: {filename} =====")
+
+        instance = read_instance(full_path)
+        if instance is None:
+            print("Skipping due to read error.")
+            continue
+
+        n, m, T, D, s_a, t_a, s_b, t_b, adj = instance
+
+        # run the algorithm
+        start_ns = time.perf_counter_ns()
+
+        dist = compute_distance_matrix(n, adj)
+        k, path_a, path_b = bfs_on_new_graph( n, adj, dist, s_a, t_a, s_b, t_b, D, T )
+
+        end_ns = time.perf_counter_ns()
+
+        running_time = (end_ns - start_ns) / 1e9
+        print("Running time =", running_time)
+
+        # retrieving the example running time from .out file
+        out_file = os.path.join(folder, filename.replace(".in", ".out"))
+
+        k_out = None
+        expected_time = None
+
+        if os.path.exists(out_file):
+            with open(out_file, "r") as f:
+                lines = f.readlines()
+
+                if len(lines) >= 1:
+                    first_line = lines[0].strip()
+                    if first_line:
+                        k_token = first_line.split()[0]
+                        k_str = k_token
+                        try:
+                            k_out = float(k_str)
+                        except ValueError:
+                            k_out = None
+
+                expected_time = None
+                for raw in reversed(lines):
+                    line = raw.strip()
+                    if not line:
+                        continue  # skip empty trailing lines
+                    first_token = line.split()[0]
+                    number_str = first_token.replace(",", ".")  # "8,9" -> "8.9"
+                    try:
+                        expected_time = float(number_str)  # 8.9
+                    except ValueError:
+                        expected_time = None
+                    break
+
+        print("expected =", expected_time)
+
+        # write the row in excel
+        ws.append([
+            filename,
+            n, m, T, D, k, k_out,
+            expected_time,
+            running_time,
+            None if expected_time is None else running_time - expected_time
+        ])
+
+    # writes an excel file with the results from each of the files in that folder
+    output_excel = "results_trash.xlsx"
+    wb.save(output_excel)
+    print("\nResults written to:", output_excel)
---
+++ solver_methods.py
@@ -0,0 +1,141 @@
+from collections import deque
+
+def bfs_on_new_graph(n, adj, dist, s_a, t_a, s_b, t_b, D, T):
+    # state = (pair << 1) | person, where pair = x * n + y, person in {0, 1}
+    # player = 0 : end of real step
+    # player = 1 : A has moved, B still has to move
+
+    N_states = n * n * 2
+
+    start_pair = s_a * n + s_b
+    goal_pair  = t_a * n + t_b
+
+    # start and goal must be valid pairs (aka distance > D)
+    d_start = dist[s_a][s_b]
+    if d_start != -1 and d_start <= D:
+        return T + 1, [], []
+
+    d_goal = dist[t_a][t_b]
+    if d_goal != -1 and d_goal <= D:
+        return T + 1, [], []
+
+    # initial and final states are at the end of a time step (player = 0)
+    start = (start_pair << 1) | 0
+    goal  = (goal_pair << 1) | 0
+
+    if start == goal:
+        return 0, [s_a + 1], [s_b + 1]
+
+
+    dist_state = [-1] * N_states
+    parent = [-1] * N_states
+
+    q = deque([start])
+    dist_state[start] = 0
+
+    #start bfs
+    while q:
+
+        # current node and distance till there
+        cur = q.popleft()
+        d_cur = dist_state[cur]
+
+        # stop if we reached the goal
+        if cur == goal:
+            break
+
+        # each bfs step is a micro-step, at most 2*T micro-steps
+        if d_cur >= 2 * T:
+            continue
+
+        #decoding
+        pair = cur >> 1
+        player = cur & 1
+        x = pair // n
+        y = pair % n
+
+        # ------ pruning if already unreachable ------
+        real_so_far = d_cur // 2
+
+        dx = dist[x][t_a]
+        dy = dist[y][t_b]
+
+        if dx == -1 or dy == -1:
+            continue
+
+        # analyzing each of the two cases
+        if player == 0:
+            h_real = max(dx, dy)
+        else:
+            h_real = max(dx, dy - 1)
+
+        if real_so_far + h_real > T:
+            continue
+        # ------------------------------------------
+
+        # which person has to move?
+        if player == 0:
+            # A moves, B stays
+            for nx in [x] + adj[x]:
+
+                #encoding
+                nxt_pair = nx * n + y
+                nxt = (nxt_pair << 1) | 1
+
+                # if not reached yet, add in the queue
+                if dist_state[nxt] != -1:
+                    continue
+                dist_state[nxt] = d_cur + 1
+                parent[nxt] = cur
+                q.append(nxt)
+
+        else:
+            # B moves, A stays
+            for ny in [y] + adj[y]:
+
+                # distance constraint
+                d_xy = dist[x][ny]
+                if d_xy != -1 and d_xy <= D:
+                    continue
+
+                # encoding
+                nxt_pair = x * n + ny
+                nxt = (nxt_pair << 1) | 0
+
+                # if not reached, add in the queue
+                if dist_state[nxt] != -1:
+                    continue
+                dist_state[nxt] = d_cur + 1
+                parent[nxt] = cur
+                q.append(nxt)
+
+    # if we never reached goal, or we exceeded T real steps, no solution
+    if dist_state[goal] == -1 or dist_state[goal] > 2 * T:
+        return T + 1, [], []
+
+    # real nr of steps
+    k = dist_state[goal] // 2
+
+    # recreating the path
+    states = []
+    cur = goal
+    while cur != -1:
+        states.append(cur)
+        cur = parent[cur]
+    states.reverse()
+
+    # we need only the positions from person = 0
+    path_a = []
+    path_b = []
+    for st in states:
+        pair = st >> 1
+        player = st & 1
+        if player != 0:
+            continue
+        x = pair // n
+        y = pair % n
+        path_a.append(x + 1)
+        path_b.append(y + 1)
+
+    return k, path_a, path_b
+
